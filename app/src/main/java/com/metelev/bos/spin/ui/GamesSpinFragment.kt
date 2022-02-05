efbc8f88f2865fb17606757c5ab419319970134f
bfmax <no>
2022-02-06 00:42:46 +0300

    добавил фрагменты экрана и открытие ссылки во вьюшке. при переходе по ней открывался сайт казино 777. Спасибо я не азартный человек, понимаю как работает казино онлайн)))

---
+++ app/src/main/java/com/metelev/bos/spin/ui/GamesSpinFragment.kt
@@ -0,0 +1,14 @@
+package com.metelev.bos.spin.ui
+
+import com.metelev.bos.spin.R
+import com.metelev.bos.spin.ui.base.BaseFragment
+
+class GamesSpinFragment : BaseFragment(R.layout.fragment_games_spin) {
+
+    override fun onResume() {
+        super.onResume()
+
+
+    }
+
+}
--- app/src/main/java/com/metelev/bos/spin/ui/MainActivity.kt
+++ app/src/main/java/com/metelev/bos/spin/ui/MainActivity.kt
@@ -9,5 +9,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.activity_main, WebFragment())
+            .commit()
     }
 }
