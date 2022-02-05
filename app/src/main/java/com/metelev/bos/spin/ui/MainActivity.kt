30fdbb4b4b66c3775054402e8e99b5c80fe7b629
bfmax <no>
2022-02-05 22:22:47 +0300

    проект

---
+++ app/src/main/java/com/metelev/bos/spin/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.metelev.bos.spin.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.metelev.bos.spin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
